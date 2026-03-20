f645474e8f2792e70f679c7461b4f29fdfcc1724
JonaBacho <110006848+JonaBacho@users.noreply.github.com>
2026-03-20 23:38:40 +0100

    Change API path from 'api' to 'fare-api'

--- fare_calculator/urls.py
+++ fare_calculator/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # API REST (authentification ApiKey via middleware)
-    path('api/', include('core.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('fare-api/', include('core.urls')),
+    path('fare-api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('fare-api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('fare-api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
