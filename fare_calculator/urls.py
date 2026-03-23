258d912f504a01a540e0115604fedf5cbc3ec02a
JonaBacho <110006848+JonaBacho@users.noreply.github.com>
2026-03-23 02:43:29 +0100

    Update urls.py

--- fare_calculator/urls.py
+++ fare_calculator/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # API REST (authentification ApiKey via middleware)
-    path('fare-api/', include('core.urls')),
-    path('fare-api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('fare-api/docs/', SpectacularSwaggerView.as_view(url='/fare-calculator/fare-api/schema/'), name='swagger-ui'),
-    path('fare-api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/', include('core.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url='/fare-calculator/fare-api/schema/'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
