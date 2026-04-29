80b67107a5770faa6fad34e13cc515bbe5533037
JonaBacho <110006848+JonaBacho@users.noreply.github.com>
2026-04-29 14:53:40 +0100

    Configure static files settings in Django
    Add STATIC_ROOT and STATICFILES_DIRS for static files management.

--- fare_calculator/settings.py
+++ fare_calculator/settings.py
@@ -214,6 +214,13 @@ USE_TZ = True
 
 STATIC_URL = '/fare-calculator/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Optionnel mais recommandé
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
